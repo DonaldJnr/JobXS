23aed64e6706739cab138e058b2c63c4c070e30b
DonaldJNRO <dokolocha101@gmail.com>
2024-06-03 13:27:17 +0100

    Okay try
    Ltey

---
+++ assets/js/main.js
@@ -0,0 +1,288 @@
+/**
+* Template Name: UpConstruction - v1.3.0
+* Template URL: https://bootstrapmade.com/upconstruction-bootstrap-construction-website-template/
+* Author: BootstrapMade.com
+* License: https://bootstrapmade.com/license/
+*/
+document.addEventListener('DOMContentLoaded', () => {
+  "use strict";
+
+  /**
+   * Preloader
+   */
+  const preloader = document.querySelector('#preloader');
+  if (preloader) {
+    window.addEventListener('load', () => {
+      preloader.remove();
+    });
+  }
+
+  /**
+   * Mobile nav toggle
+   */
+
+  const mobileNavShow = document.querySelector('.mobile-nav-show');
+  const mobileNavHide = document.querySelector('.mobile-nav-hide');
+
+  document.querySelectorAll('.mobile-nav-toggle').forEach(el => {
+    el.addEventListener('click', function(event) {
+      event.preventDefault();
+      mobileNavToogle();
+    })
+  });
+
+  function mobileNavToogle() {
+    document.querySelector('body').classList.toggle('mobile-nav-active');
+    mobileNavShow.classList.toggle('d-none');
+    mobileNavHide.classList.toggle('d-none');
+  }
+
+  /**
+   * Hide mobile nav on same-page/hash links
+   */
+  document.querySelectorAll('#navbar a').forEach(navbarlink => {
+
+    if (!navbarlink.hash) return;
+
+    let section = document.querySelector(navbarlink.hash);
+    if (!section) return;
+
+    navbarlink.addEventListener('click', () => {
+      if (document.querySelector('.mobile-nav-active')) {
+        mobileNavToogle();
+      }
+    });
+
+  });
+
+  /**
+   * Toggle mobile nav dropdowns
+   */
+  const navDropdowns = document.querySelectorAll('.navbar .dropdown > a');
+
+  navDropdowns.forEach(el => {
+    el.addEventListener('click', function(event) {
+      if (document.querySelector('.mobile-nav-active')) {
+        event.preventDefault();
+        this.classList.toggle('active');
+        this.nextElementSibling.classList.toggle('dropdown-active');
+
+        let dropDownIndicator = this.querySelector('.dropdown-indicator');
+        dropDownIndicator.classList.toggle('bi-chevron-up');
+        dropDownIndicator.classList.toggle('bi-chevron-down');
+      }
+    })
+  });
+
+  /**
+   * Scroll top button
+   */
+  const scrollTop = document.querySelector('.scroll-top');
+  if (scrollTop) {
+    const togglescrollTop = function() {
+      window.scrollY > 100 ? scrollTop.classList.add('active') : scrollTop.classList.remove('active');
+    }
+    window.addEventListener('load', togglescrollTop);
+    document.addEventListener('scroll', togglescrollTop);
+    scrollTop.addEventListener('click', window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    }));
+  }
+
+  /**
+   * Initiate glightbox
+   */
+  const glightbox = GLightbox({
+    selector: '.glightbox'
+  });
+
+  /**
+   * Porfolio isotope and filter
+   */
+  let portfolionIsotope = document.querySelector('.portfolio-isotope');
+
+  if (portfolionIsotope) {
+
+    let portfolioFilter = portfolionIsotope.getAttribute('data-portfolio-filter') ? portfolionIsotope.getAttribute('data-portfolio-filter') : '*';
+    let portfolioLayout = portfolionIsotope.getAttribute('data-portfolio-layout') ? portfolionIsotope.getAttribute('data-portfolio-layout') : 'masonry';
+    let portfolioSort = portfolionIsotope.getAttribute('data-portfolio-sort') ? portfolionIsotope.getAttribute('data-portfolio-sort') : 'original-order';
+
+    window.addEventListener('load', () => {
+      let portfolioIsotope = new Isotope(document.querySelector('.portfolio-container'), {
+        itemSelector: '.portfolio-item',
+        layoutMode: portfolioLayout,
+        filter: portfolioFilter,
+        sortBy: portfolioSort
+      });
+
+      let menuFilters = document.querySelectorAll('.portfolio-isotope .portfolio-flters li');
+      menuFilters.forEach(function(el) {
+        el.addEventListener('click', function() {
+          document.querySelector('.portfolio-isotope .portfolio-flters .filter-active').classList.remove('filter-active');
+          this.classList.add('filter-active');
+          portfolioIsotope.arrange({
+            filter: this.getAttribute('data-filter')
+          });
+          if (typeof aos_init === 'function') {
+            aos_init();
+          }
+        }, false);
+      });
+
+    });
+
+  }
+
+  /**
+   * Init swiper slider with 1 slide at once in desktop view
+   */
+  new Swiper('.slides-1', {
+    speed: 600,
+    loop: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false
+    },
+    slidesPerView: 'auto',
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+      clickable: true
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    }
+  });
+
+  /**
+   * Init swiper slider with 2 slides at once in desktop view
+   */
+  new Swiper('.slides-2', {
+    speed: 600,
+    loop: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false
+    },
+    slidesPerView: 'auto',
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+      clickable: true
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    breakpoints: {
+      320: {
+        slidesPerView: 1,
+        spaceBetween: 20
+      },
+
+      1200: {
+        slidesPerView: 2,
+        spaceBetween: 20
+      }
+    }
+  });
+
+  /**
+   * Initiate pURE cOUNTER
+   */
+  new PureCounter();
+
+  /**
+   * Animation on scroll function and init
+   */
+  function aos_init() {
+    AOS.init({
+      duration: 800,
+      easing: 'slide',
+      once: true,
+      mirror: false
+    });
+  }
+  window.addEventListener('load', () => {
+    aos_init();
+  });
+
+  var countDownDate = new Date("November 1, 2024 23:59:59").getTime();
+
+  // Function to update the countdown number with animation only if the number has changed
+  function updateNumber(elementId, value) {
+      var element = document.getElementById(elementId);
+      var currentValue = element.textContent || element.innerText;
+  
+      if (currentValue !== value) {
+          // Trigger reflow to restart the animation
+          element.classList.remove('countdown-animation');
+          void element.offsetWidth; // reflow
+  
+          // Update the number and re-add the animation class
+          element.textContent = value; // For modern browsers
+          element.innerText = value; // For IE
+          element.classList.add('countdown-animation');
+      }
+  }
+  
+  // Update the countdown every second
+  var countdownFunction = setInterval(function() {
+      var now = new Date().getTime();
+      var timeLeft = countDownDate - now;
+  
+      if (timeLeft >= 0) {
+          var days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
+          var hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+          var minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
+          var seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
+  
+          // Format with leading zero and update the countdown
+          updateNumber("days", days < 10 ? "0" + days : days.toString());
+          updateNumber("hours", hours < 10 ? "0" + hours : hours.toString());
+          updateNumber("minutes", minutes < 10 ? "0" + minutes : minutes.toString());
+          updateNumber("seconds", seconds < 10 ? "0" + seconds : seconds.toString());
+      } else {
+          // If the countdown is finished, clear the interval and set the numbers to 00
+          clearInterval(countdownFunction);
+          updateNumber("days", "00");
+          updateNumber("hours", "00");
+          updateNumber("minutes", "00");
+          updateNumber("seconds", "00");
+      }
+  }, 1000);
+  
+  document.addEventListener('DOMContentLoaded', function () {
+    VanillaTilt.init(document.querySelectorAll(".card-item"), {
+      max: 25,  // Maximum tilt rotation in degrees
+      speed: 400,  // Speed of the enter/exit effect
+      glare: true,  // Adds a "glare" effect
+      "max-glare": 0.5,  // Maximum "glare" opacity (between 0 and 1)
+    });
+  });
+  
+  document.querySelectorAll('.faq-question').forEach(question => {
+    question.addEventListener('click', function() {
+        // This will find the next sibling with the class 'faq-answer' and toggle it
+        this.nextElementSibling.style.display = this.nextElementSibling.style.display === 'block' ? 'none' : 'block';
+    });
+});
+
+document.querySelectorAll('.service-item').forEach(item => {
+  const video = item.querySelector('.card-video');
+
+  item.addEventListener('mouseenter', () => {
+    video.play();
+  });
+
+  item.addEventListener('mouseleave', () => {
+    video.pause();
+    video.currentTime = 0; // Optionally, rewind the video
+  });
+});
+
+
+
+});
